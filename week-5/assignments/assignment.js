9c3f9c58f4f1a87fdc9c703fe0cf5021c9dd7632
pooja-9810 <n01496379@humbermail.ca>
2022-02-08 22:34:36 -0500

    Assignment for Week 5

---
+++ week-5/assignments/assignment.js
@@ -0,0 +1,137 @@
+  
+const renderTable = () => {
+    const table = document.getElementById("people");
+const htmlToElement = (html) => {
+    const template = document.createElement("template");
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+  };
+
+  
+    if (!table) {
+      throw new Error("Cannot find table element");
+    }
+  
+    var data = [
+      {
+        id: 1,
+        first_name: "Husain",
+        last_name: "Zeale",
+        email: "hzeale0@odnoklassniki.ru",
+        gender: "Male",
+        ip_address: "143.237.191.245",
+      },
+      {
+        id: 2,
+        first_name: "Michale",
+        last_name: "Delgado",
+        email: "mdelgado1@paginegialle.it",
+        gender: "Male",
+        ip_address: "144.2.126.19",
+      },
+      {
+        id: 3,
+        first_name: "Gaspard",
+        last_name: "Upwood",
+        email: "gupwood2@fastcompany.com",
+        gender: "Male",
+        ip_address: "147.85.27.165",
+      },
+      {
+        id: 4,
+        first_name: "Gussy",
+        last_name: "Dowzell",
+        email: "gdowzell3@ox.ac.uk",
+        gender: "Male",
+        ip_address: "22.201.167.239",
+      },
+      {
+        id: 5,
+        first_name: "Alida",
+        last_name: "Macias",
+        email: "amacias4@slate.com",
+        gender: "Female",
+        ip_address: "166.62.41.24",
+      },
+      {
+        id: 6,
+        first_name: "Tess",
+        last_name: "Sainte Paul",
+        email: "tsaintepaul5@xinhuanet.com",
+        gender: "Female",
+        ip_address: "58.80.74.111",
+      },
+      {
+        id: 7,
+        first_name: "Germaine",
+        last_name: "Winnett",
+        email: "gwinnett6@parallels.com",
+        gender: "Genderqueer",
+        ip_address: "224.15.42.167",
+      },
+      {
+        id: 8,
+        first_name: "Mei",
+        last_name: "Swansbury",
+        email: "mswansbury7@uol.com.br",
+        gender: "Female",
+        ip_address: "191.235.203.177",
+      },
+      {
+        id: 9,
+        first_name: "Brietta",
+        last_name: "Greenfield",
+        email: "bgreenfield8@qq.com",
+        gender: "Male",
+        ip_address: "72.108.253.70",
+      },
+      {
+        id: 10,
+        first_name: "Guinna",
+        last_name: "Dagnan",
+        email: "gdagnan9@thetimes.co.uk",
+        gender: "Female",
+        ip_address: "108.233.202.64",
+      },
+    ];
+  
+    const addfilter = Boolean(window.location.search);
+    if (addfilter) {
+      const params = new URLSearchParams(window.location.search);
+      const name = params.get(`name`).toLowerCase();
+      const inputControl = document.getElementById(`inputValue`);
+      inputControl.value = name;
+  
+      data = data.filter(
+        ({ first_name, last_name, email, gender, ip_address }) =>
+          last_name.toLowerCase().includes(name) ||
+          first_name.toLowerCase().includes(name) ||
+          email.toLowerCase().includes(name) ||
+          gender.toLowerCase().includes(name) ||
+          ip_address.includes(name)
+      );
+    }
+  
+    const rows = data.map(
+      ({ id, first_name, last_name, email, gender, ip_address }) =>
+        table.appendChild(
+          htmlToElement(
+            `<tr id="table-row-${id}">
+            <td>${id}</td>
+            <td>${first_name}</td>
+            <td>${last_name}</td>
+            <td>${email}</td>
+            <td>${gender}</td>
+            <td>${ip_address}</td>
+            </tr>`
+          )
+        )
+    );
+  };
+  
+  renderTable();
+  
+  const onReset = () => {
+    window.location.replace(window.location.pathname);
+  };
